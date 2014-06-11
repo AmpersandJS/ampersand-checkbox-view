2ee880ea13583d698b7856bfba99d7248f585266
Henrik Joreteg <hjoreteg@gmail.com>
2014-06-10 22:02:50 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# ampersand-checkbox-view
+
+A view module for intelligently rendering and validating checkbox input. Works well with [ampersand-form-view](ampersandjs/ampersand-form-view).
+
+It does the following:
+
+- Automatically shows/hides error message based required.
+- Will only show error message checkbox is required and:
+    - trying to submit form and it's not checked.
+    - it has ever been checked and now isn't.
+
+Depdent on two micro-libs, so works well with ampersand apps, backbone apps or anything else, really.
+
+
+## install
+
+```
+npm install ampersand-checkbox-view
+```
+
+## example
+
+```javascript
+var CheckboxView = require('ampersand-checkbox-view');
+
+var field = new CheckboxView({
+    // form input `name`
+    name: 'client_name',
+    // You can replace the built-in template with your own.
+    // just give it an html string. Make sure it has a single "root" element that contains:
+    //  - an `<input>` element
+    //  - an element with a `role="label"` attribute
+    //  - an element with a `role="message-container"` attribute (this we'll show/hide)
+    //  - an elememt with a `role="message-text"` attribute (where message text goes for error)
+    template: // some HTML string
+    // Label name
+    label: 'App Name',
+    // optinal intial value if it has one
+    value: true, // or false
+    // optional, this is the element that will be 
+    // replaced by this view. If you don't
+    // give it one, it will create one.
+    el: document.getElementByID('field'),
+    // whether or not this field is required
+    required: true, // true by default
+    // class to set on input when input is valid
+    validClass: 'input-valid', // <- that's the default
+    // type value to use for the input tag's type value
+    type: 'text',
+    // class to set on input when input is valid
+    invalidClass: 'input-invalid', // <- that's the default
+    // Message to use if error is that it's required
+    // but no value was set.
+    requiredMessage: 'This box must be checked.',
+    // optional, you can pass in the parent view explicitly
+    parent:  someViewInstance 
+});
+
+// append it somewhere or use it in side an ampersand-form-view
+document.querySelector('form').appendChild(field.el);
+
+```
+
+## credits
+
+Created by [@HenrikJoreteg](http://twitter.com/henrikjoreteg).
+
+## license
+
+MIT
+
---
+++ ampersand-checkbox-view.js
@@ -0,0 +1,133 @@
+var domify = require('domify');
+var dom = require('ampersand-dom');
+
+
+// can be overwritten by anything with:
+// <input>, <label> and <the same `role` attributes
+var template = [
+    '<label class="checkbox">',
+        '<input type="checkbox"><span role="label"></span>',
+        '<div role="message-container" class="message message-below message-error">',
+            '<p role="message-text"></p>',
+        '</div>',
+    '</label>'
+].join('');
+
+
+function BooleanInputView(opts) {
+    if (!opts.name) throw new Error('must pass in a name');
+
+    // settings
+    this.name = opts.name;
+    this.value = opts.value || '';
+    this.el = opts.el;
+    this.template = opts.template || template;
+    this.label = opts.label || options.name;
+    this.required = (typeof opts.required === 'boolean') ? opts.required : false;
+    this.validClass = opts.validClass || 'input-valid';
+    this.invalidClass = opts.invalidClass || 'input-invalid';
+    this.requiredMessage = opts.requiredMessage || 'This box must be checked.';
+    this.parent = opts.parent;
+
+    // render right away
+    this.render();
+
+    // bind event handlers
+    this.handleInputEvent = this.handleInputEvent.bind(this);
+
+    // add our event handlers
+    this.input.addEventListener('change', this.handleInputEvent, false);
+
+    if (opts.value) this.setValue(true);
+
+    if (this.isValid()) {
+        this.hasBeenValid = true;
+    }
+    // always start true with checkboxes
+    this.valid = true;
+}
+
+// remove and destroy element
+BooleanInputView.prototype.remove = function () {
+    this.input.removeEventListener('change', this.handleInputEvent, false);
+    var parent = this.el.parentNode;
+    if (parent) parent.removeChild(this.el);
+};
+
+// very `manual` render to avoid dependencies
+BooleanInputView.prototype.render = function () {
+    // only allow this to be called once
+    if (this.rendered) return;
+    var newDom = domify(this.template);
+    var parent = this.el && this.el.parentNode;
+    if (parent) parent.replaceChild(newDom, this.el);
+    this.el = newDom;
+    this.input = this.el.querySelector('input');
+    this.labelEl = this.el.querySelector('[role=label]');
+    this.messageContainer = this.el.querySelector('[role=message-container]');
+    this.messageEl = this.el.querySelector('[role=message-text]');
+    this.setMessage(this.message);
+    if (this.required) this.input.required = true;
+    this.input.value = !!this.value;
+    this.labelEl.textContent = this.label;
+    this.rendered = true;
+};
+
+// handle input events and show appropriate errors
+BooleanInputView.prototype.handleInputEvent = function () {
+    // track whether user has edited directly
+    if (document.activeElement === this.input) this.directlyEdited = true;
+    this.value = this.input.checked;
+    if (this.message || this.hasBeenValid) {
+        this.valid = this.runTests();
+    }
+    if (this.parent) this.parent.update(this);
+};
+
+// set the error message if exists
+// hides the message container entirely otherwise
+BooleanInputView.prototype.setMessage = function (message) {
+    var input = this.input;
+    this.message = message;
+    // there is an error
+    if (message && this.hasBeenValid) {
+        this.messageContainer.style.display = 'block';
+        this.messageEl.textContent = message;
+        dom.addClass(input, this.invalidClass);
+        dom.removeClass(input, this.validClass);
+    } else {
+        this.messageContainer.style.display = 'none';
+        if (this.hasBeenValid && this.editedDirectly) {
+            dom.addClass(input, this.validClass);
+            dom.removeClass(input, this.invalidClass);
+        }
+    }
+};
+
+BooleanInputView.prototype.setValue = function (value) {
+    this.input.checked = !!value;
+    this.handleInputEvent();
+};
+
+BooleanInputView.prototype.beforeSubmit = function () {
+    this.hasBeenValid = true;
+    this.valid = this.runTests();
+    this.handleInputEvent();
+};
+
+BooleanInputView.prototype.isValid = function () {
+    return !!(!this.required || this.input.checked);
+};
+
+// runs tests and sets first failure as message
+BooleanInputView.prototype.runTests = function () {
+    var valid = this.isValid();
+    if (valid) {
+        this.hasBeenValid = true;
+    }
+    this.setMessage(valid ? '' : this.requiredMessage);
+    return valid;
+};
+
+
+module.exports = BooleanInputView;
