4c62642de35a8f025028d83f92b2b79057f50d93
cdaringe <cdieringer@mrn.org>
2015-04-11 14:38:52 -0700

    3.0.0 rc

--- README.md
+++ README.md
@@ -6,14 +6,14 @@ Lead Maintainer: [Michael Garvin](https://github.com/wraithgar)
 
 A view module for intelligently rendering and validating checkbox input. Works well with [ampersand-form-view](ampersandjs/ampersand-form-view).
 
+[ampersand-checkbox-view](#ampersand-checkbox-view) extends [ampersand-view](ampersandjs/ampersand-view), so you may further `.extend({...})` it to your desire.
+
 It does the following:
 
-- Automatically shows/hides error message based required.
+- Automatically shows/hides error message based on if the field is [`required`](#example).
 - Will only show error message checkbox is required and:
     - trying to submit form and it's not checked.
     - it has ever been checked and now isn't.
-
-Depdent on two micro-libs, so works well with ampersand apps, backbone apps or anything else, really.
 
 <!-- starthide -->
 Part of the [Ampersand.js toolkit](http://ampersandjs.com) for building clientside applications.
@@ -44,7 +44,7 @@ var field = new CheckboxView({
     label: 'App Name',
     // optinal intial value if it has one
     value: true, // or false
-    // optional, this is the element that will be 
+    // optional, this is the element that will be
     // replaced by this view. If you don't
     // give it one, it will create one.
     el: document.getElementByID('field'),
@@ -58,7 +58,9 @@ var field = new CheckboxView({
     // but no value was set.
     requiredMessage: 'This box must be checked.',
     // optional, you can pass in the parent view explicitly
-    parent:  someViewInstance 
+    parent:  someViewInstance,
+    // optional, called before form's submitCallback function called
+    beforeSubmit: function() { console.log('ampersand-checkbox-view rocks!'); }
 });
 
 // append it somewhere or use it in side an ampersand-form-view
@@ -66,9 +68,32 @@ document.querySelector('form').appendChild(field.el);
 
 ```
 
-## browser support 
+## api
+### properties
+Commonly accessed properties listed below.  See the `initialize()` function for additional properties available.
+
+- `valid` (boolean)
+- `value` (boolean)
+- `startingValue` (boolean) - this is coerced from any initail `value` provided during construction
+
+### functions
+- `new CheckboxView()` - constructor
+    - see the [example](#example) for contructor options
+- `clear()`
+    - sugar for `view.setValue(false);`
+- `reset()`
+    - sugar for `view.setValue(this.startingValue);`
+- `setValue([boolean])`
+    - sets the value of the view. the view will **always** maintain a value of type `boolean`.
+
+## browser support
 
 [![testling badge](https://ci.testling.com/AmpersandJS/ampersand-checkbox-view.png)](https://ci.testling.com/AmpersandJS/ampersand-checkbox-view)
+
+## changelog
+
+- 3.0.0
+    - Extend ampersand-view.  Add support `autoRender`, `clear`, `reset`
 
 ## credits
 
--- ampersand-checkbox-view.js
+++ ampersand-checkbox-view.js
@@ -1,4 +1,5 @@
 /*$AMPERSAND_VERSION*/
+var View = require('ampersand-view');
 var domify = require('domify');
 var dom = require('ampersand-dom');
 
@@ -14,112 +15,105 @@ var template = [
     '</label>'
 ].join('');
 
+module.exports = View.extend({
+    initialize: function(opts) {
+        if (!opts || !opts.name) throw new Error('must pass in a name');
 
-function CheckboxView(opts) {
-    if (!opts || !opts.name) throw new Error('must pass in a name');
+        // settings
+        this.el = opts.el;
+        this.name = opts.name;
+        this.startingValue = !!opts.value;
+        this.value = this.startingValue;
+        this.label = opts.label || opts.name;
+        this.required = (typeof opts.required === 'boolean') ? opts.required : false;
+        this.validClass = opts.validClass || 'input-valid';
+        this.invalidClass = opts.invalidClass || 'input-invalid';
+        this.requiredMessage = opts.requiredMessage || 'This box must be checked.';
+        this.parent = opts.parent || this.parent;
+        this.autoRender = opts.autoRender;
+        this.template = opts.template || template;
+        this.beforeSubmit = opts.beforeSubmit || this.beforeSubmit;
 
-    // settings
-    this.name = opts.name;
-    this.value = (opts.value === true) || false;
-    this.el = opts.el;
-    this.template = opts.template || template;
-    this.label = opts.label || opts.name;
-    this.required = (typeof opts.required === 'boolean') ? opts.required : false;
-    this.validClass = opts.validClass || 'input-valid';
-    this.invalidClass = opts.invalidClass || 'input-invalid';
-    this.requiredMessage = opts.requiredMessage || 'This box must be checked.';
-    this.parent = opts.parent;
+        this.setValue(this.value);
+    },
+    clear: function () {
+        return this.setValue(false);
+    },
+    reset: function () {
+        return this.setValue(this.startingValue);
+    },
+    remove: function () {
+        if (this.input) this.input.removeEventListener('change', this.handleInputEvent, false);
+        View.prototype.remove.call(this);
+    },
+    render: function () {
+        // only allow this to be called once
+        if (this.rendered) return;
+        var newDom = domify(this.template);
+        var parent = this.el && this.el.parentNode;
+        if (parent) parent.replaceChild(newDom, this.el);
+        this.el = newDom;
+        this.input = this.el.querySelector('input');
+        this.labelEl = this.el.querySelector('[data-hook~=label]');
+        this.messageContainer = this.el.querySelector('[data-hook~=message-container]');
+        this.messageEl = this.el.querySelector('[data-hook~=message-text]');
 
-    // render right away
-    this.render();
+        // bind event handlers
+        this.handleInputEvent = this.handleInputEvent.bind(this);
 
-    // bind event handlers
-    this.handleInputEvent = this.handleInputEvent.bind(this);
+        // add our event handlers
+        this.input.addEventListener('change', this.handleInputEvent, false);
 
-    // add our event handlers
-    this.input.addEventListener('change', this.handleInputEvent, false);
-
-    if (opts.value) this.setValue(true);
-
-    this.test();
-}
-
-// remove and destroy element
-CheckboxView.prototype.remove = function () {
-    this.input.removeEventListener('change', this.handleInputEvent, false);
-    var parent = this.el.parentNode;
-    if (parent) parent.removeChild(this.el);
-};
-
-// very `manual` render to avoid dependencies
-CheckboxView.prototype.render = function () {
-    // only allow this to be called once
-    if (this.rendered) return;
-    var newDom = domify(this.template);
-    var parent = this.el && this.el.parentNode;
-    if (parent) parent.replaceChild(newDom, this.el);
-    this.el = newDom;
-    this.input = this.el.querySelector('input');
-    this.labelEl = this.el.querySelector('[data-hook~=label]');
-    this.messageContainer = this.el.querySelector('[data-hook~=message-container]');
-    this.messageEl = this.el.querySelector('[data-hook~=message-text]');
-    this.setMessage(this.message);
-    this.input.checked = !!this.value;
-    this.input.name = this.name;
-    this.labelEl.textContent = this.label;
-    this.rendered = true;
-};
-
-// handle input events and show appropriate errors
-CheckboxView.prototype.handleInputEvent = function () {
-    // track whether user has edited directly
-    if (document.activeElement === this.input) this.directlyEdited = true;
-    this.value = this.input.checked;
-    this.test();
-    if (this.parent) this.parent.update(this);
-};
-
-// set the error message if exists
-// hides the message container entirely otherwise
-CheckboxView.prototype.setMessage = function (message) {
-    var input = this.input;
-    this.message = message;
-    // there is an error
-    if (message && this.shouldValidate) {
-        this.messageContainer.style.display = 'block';
-        this.messageEl.textContent = message;
-        dom.addClass(input, this.invalidClass);
-        dom.removeClass(input, this.validClass);
-    } else {
-        this.messageContainer.style.display = 'none';
-        if (this.shouldValidate && this.editedDirectly) {
-            dom.addClass(input, this.validClass);
-            dom.removeClass(input, this.invalidClass);
+        this.setMessage(this.message);
+        this.input.checked = !!this.value;
+        this.input.name = this.name;
+        this.labelEl.textContent = this.label;
+    },
+    // handle input events and show appropriate errors
+    handleInputEvent: function () {
+        // track whether user has edited directly
+        if (document.activeElement === this.input) this.directlyEdited = true;
+        this.value = this.input ? this.input.checked : this.value;
+        this.test();
+        if (this.parent) this.parent.update(this);
+    },
+    // set the error message if exists
+    // hides the message container entirely otherwise
+    setMessage: function (message) {
+        var input = this.input;
+        if (!input) return;
+        this.message = message;
+        // there is an error
+        if (message && this.shouldValidate) {
+            this.messageContainer.style.display = 'block';
+            this.messageEl.textContent = message;
+            dom.addClass(input, this.invalidClass);
+            dom.removeClass(input, this.validClass);
+        } else {
+            this.messageContainer.style.display = 'none';
+            if (this.shouldValidate && this.editedDirectly) {
+                dom.addClass(input, this.validClass);
+                dom.removeClass(input, this.invalidClass);
+            }
         }
+    },
+    setValue: function (value) {
+        if (this.input) this.input.checked = !!value;
+        return this.handleInputEvent();
+    },
+    beforeSubmit: function () {
+        this.shouldValidate = true;
+        return this.handleInputEvent();
+    },
+    test: function () {
+        var valid = !this.required || !!this.value;
+        if (valid) this.shouldValidate = true;
+        this.valid = valid;
+        if (this.shouldValidate && !valid) {
+            this.setMessage(this.requiredMessage);
+        } else {
+            this.setMessage('');
+        }
+        return valid;
     }
-};
-
-CheckboxView.prototype.setValue = function (value) {
-    this.input.checked = !!value;
-    this.handleInputEvent();
-};
-
-CheckboxView.prototype.beforeSubmit = function () {
-    this.shouldValidate = true;
-    this.handleInputEvent();
-};
-
-CheckboxView.prototype.test = function () {
-    var valid = !this.required || this.value;
-    if (valid) this.shouldValidate = true;
-    this.valid = valid;
-    if (this.shouldValidate && !valid) {
-        this.setMessage(this.requiredMessage);
-    } else {
-        this.setMessage('');
-    }
-    return valid;
-};
-
-
-module.exports = CheckboxView;
+});
